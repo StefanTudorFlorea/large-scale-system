41fe1c8361af1c9d00f55d5142245d649086a993
StefanFlorea <stefanflorea.tudor@gmail.com>
2024-01-09 12:26:40 +0100

    Restructure project

--- docker/services/docker-entry.sh
+++ docker/services/docker-entry.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo  "[ERROR]: Missing service id argument"
-    exit -1
+    echo  "Building services"
+    exit 0
 else
-    if [ "$1" == "bash" ]; then
-	exec "/bin/bash"
-    fi
-
     SERVICE_ID=$1
     echo "Running service '${SERVICE_ID}'"
 fi
